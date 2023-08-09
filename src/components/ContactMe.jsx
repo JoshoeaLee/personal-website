2c1a58ce899dd778c39f8bf37ab33e39d7b6069c
Joshua Lee <joshleecloud@gmail.com>
2023-08-09 22:38:55 +1200

    Fixed CV

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -10,7 +10,7 @@ export default function ContactMe(){
             <Stack id='contact' gap={3} direction='horizontal' >
                     <a href="https://github.com/JoshoeaLee"><img src={github} alt="Github" style={{height: '50%'}} /></a>
                     <a href="https://linkedin.com/in/joshua-lee97"><img src={linkedin} alt="Linkedin" style={{height: '50%'}} /></a>
-                    <a href="https://joshportfolioprojectpics.blob.core.windows.net/project-images/Resume-2.pdf"><img src={resume} alt="My CV" style={{height: '50%'}} /></a>
+                    <a href="https://raw.githubusercontent.com/JoshoeaLee/Assets/master/JoshuaLee_CV.pdf"><img src={resume} alt="My CV" style={{height: '50%'}} /></a>
             </Stack>
             <h6 className="muted">Email:me@joshualee.co.nz</h6> 
 
